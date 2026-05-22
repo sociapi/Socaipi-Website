5e77c16d59de458d85634b751c2afcf11c54442c
zuhairzeb <Zebzuhair71@gmail.com>
2026-05-22 12:47:12 +0500

    Update Next.js

--- Sociapi Society/src/components/Next.js
+++ Sociapi Society/src/components/Next.js
@@ -1,1 +1,13 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import type { AppProps } from 'next/app';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+ 
+export default MyApp;
